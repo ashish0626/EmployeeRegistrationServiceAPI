f0d2cc17d5320dc31862736d5186cf5123793cdb
Ashishkumar Mangaldas Chourasia <ashishkumar.mangaldas-chourasia@hpe.com>
2021-12-14 20:11:22 +0530

    Auth Token UI Added

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace EmployeeRegistrationService
 {
@@ -22,14 +27,55 @@ namespace EmployeeRegistrationService
         {
             services.AddControllers();
             services.AddSingleton<IPlaceInfoService, PlaceInfoService>();
-            services.AddSwaggerGen(options =>
+            //services.AddSwaggerGen(options =>
+            //{
+            //    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+            //    {
+            //        Title = "Place Info Service API",
+            //        Version = "v2",
+            //        Description = "Sample service for Learner",
+            //    });
+            //});
+            services.AddSwaggerGen(c =>
             {
-                options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
                 {
                     Title = "Place Info Service API",
                     Version = "v2",
                     Description = "Sample service for Learner",
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                  new OpenApiSecurityScheme
+                  {
+                    Reference = new OpenApiReference
+                      {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                      },
+                      Scheme = "oauth2",
+                      Name = "Bearer",
+                      In = ParameterLocation.Header,
+
+                    },
+                    new List<string>()
+                  }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
             });
         }
 
